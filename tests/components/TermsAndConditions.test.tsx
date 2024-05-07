3b8af10d1948e564a874fe2f2f529b8328dc213f
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2024-05-07 13:11:13 +0500

    intial commit

---
+++ tests/components/TermsAndConditions.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import TermsAndConditions from "../../src/components/TermsAndConditions";
+import userEvent from "@testing-library/user-event";
+
+describe("TermsAndConditions", () => {
+  const renderComponent = () => {
+    render(<TermsAndConditions />);
+
+    return {
+      heading: screen.getByRole("heading"),
+      checkbox: screen.getByRole("checkbox"),
+      button: screen.getByRole("button"),
+    };
+  };
+
+  it("should render with correct text and initial state", () => {
+    const { heading, checkbox, button } = renderComponent();
+
+    expect(heading).toHaveTextContent("Terms & Conditions");
+    expect(checkbox).not.toBeChecked();
+    expect(button).toBeDisabled();
+  });
+});
